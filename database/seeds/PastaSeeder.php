43b58d041b2120f73d461c4d7f84f1acb1e1d8ee
AndreaDeCeglia <deceglia89@gmail.com>
2023-01-23 17:12:46 +0100

    migration, seeder, and model done

---
+++ database/seeds/PastaSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PastaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
